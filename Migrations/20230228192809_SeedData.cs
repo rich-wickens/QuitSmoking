eb6ad051ecce63f12f229144aafb6db86ff7b6ee
Richard Wickens <Rwickens100@gmail.com>
2023-02-28 19:51:21 +0000

    Added sqlite database, added User class, added UserDataContext class.
    Steps 1-3 from https://www.allhandsontech.com/programming/blazor/how-to-sqlite-blazor-4 [last accessed 28/02/2023]

---
+++ Migrations/20230228192809_SeedData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuitSmoking.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Name", "Password", "UserName" },
+                values: new object[] { 1, "mlerego0@marketwatch.com", "Maure Lerego", "cCTDKwb11r", "mlerego0" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
