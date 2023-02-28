4fb9feb8c01eb7b0e6f6f50265440cd761368f8e
Richard Wickens <Rwickens100@gmail.com>
2023-02-28 22:20:04 +0000

    Added Read operation for database

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,52 @@
+﻿using QuitSmoking.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace QuitSmoking.Pages
+{
+    public partial class Index
+    {
+        public bool ShowCreate { get; set; }
+        private UserDataContext? _context;
+        public User? NewUser { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            ShowCreate = false;
+            await ShowUsers();
+        }
+
+        public void ShowCreateForm()
+        {
+            NewUser = new User();
+            ShowCreate = true;
+        }
+
+        public async Task CreateNewUser()
+        {
+            _context ??= await UserDataContextFactory.CreateDbContextAsync();
+
+            if (NewUser is not null)
+            {
+                _context?.Users.Add(NewUser);
+                _context?.SaveChangesAsync();
+            }
+
+            ShowCreate = false;
+        }
+
+        public List<User>? OurUsers { get; set; }
+
+        public async Task ShowUsers()
+        {
+            _context ??= await UserDataContextFactory.CreateDbContextAsync();
+
+            if (_context is not null)
+            {
+                OurUsers = await _context.Users.ToListAsync();
+            }
+
+            if (_context is not null) await _context.DisposeAsync();
+        }
+    }
+
+}
